646d95c06fcaf2d8aa3e196b229734a893a181a9
gshabtai <74379758+gshabtai@users.noreply.github.com>
2022-03-01 12:29:14 -0800

    Add files via upload

---
+++ nn_mask_detector_raw.py
@@ -0,0 +1,300 @@
+# This is the raw code from the JupyterLab notebook.
+
+from jetcam.usb_camera import USBCamera
+from jetcam.csi_camera import CSICamera
+
+# for USB Camera (Logitech C270 webcam), uncomment the following line
+camera = USBCamera(width=224, height=224, capture_device=0) # confirm the capture_device number
+
+# for CSI Camera (Raspberry Pi Camera Module V2), uncomment the following line
+# camera = CSICamera(width=224, height=224, capture_device=0) # confirm the capture_device number
+
+camera.running = True
+print("camera created")
+
+import torchvision.transforms as transforms
+from dataset import ImageClassificationDataset
+
+TASK = 'masked'
+
+CATEGORIES = ['no_mask', 'bad_mask', 'good_mask']
+
+DATASETS = ['A'] 
+
+TRANSFORMS = transforms.Compose([
+    transforms.ColorJitter(0.2, 0.2, 0.2, 0.2),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+datasets = {}
+for name in DATASETS:
+    datasets[name] = ImageClassificationDataset('../data/classification/' + TASK + '_' + name, CATEGORIES, TRANSFORMS)
+    
+print("{} task with {} categories defined".format(TASK, CATEGORIES))
+
+import ipywidgets
+import traitlets
+from IPython.display import display
+from jetcam.utils import bgr8_to_jpeg
+
+# initialize active dataset
+dataset = datasets[DATASETS[0]]
+
+# unobserve all callbacks from camera in case we are running this cell for second time
+camera.unobserve_all()
+
+# create image preview
+camera_widget = ipywidgets.Image()
+traitlets.dlink((camera, 'value'), (camera_widget, 'value'), transform=bgr8_to_jpeg)
+
+# create widgets
+dataset_widget = ipywidgets.Dropdown(options=DATASETS, description='dataset')
+category_widget = ipywidgets.Dropdown(options=dataset.categories, description='category')
+count_widget = ipywidgets.IntText(description='count')
+save_widget = ipywidgets.Button(description='add')
+
+# manually update counts at initialization
+count_widget.value = dataset.get_count(category_widget.value)
+
+# sets the active dataset
+def set_dataset(change):
+    global dataset
+    dataset = datasets[change['new']]
+    count_widget.value = dataset.get_count(category_widget.value)
+dataset_widget.observe(set_dataset, names='value')
+
+# update counts when we select a new category
+def update_counts(change):
+    count_widget.value = dataset.get_count(change['new'])
+category_widget.observe(update_counts, names='value')
+
+# save image for category and update counts
+def save(c):
+    dataset.save_entry(camera.value, category_widget.value)
+    count_widget.value = dataset.get_count(category_widget.value)
+save_widget.on_click(save)
+
+data_collection_widget = ipywidgets.VBox([
+    ipywidgets.HBox([camera_widget]), dataset_widget, category_widget, count_widget, save_widget
+])
+
+# display(data_collection_widget)
+print("data_collection_widget created")
+
+import torch
+import torchvision
+
+
+device = torch.device('cuda')
+
+# RESNET 18
+model = torchvision.models.resnet18(pretrained=True)
+model.fc = torch.nn.Linear(512, len(dataset.categories))
+    
+model = model.to(device)
+
+model_save_button = ipywidgets.Button(description='save model')
+model_load_button = ipywidgets.Button(description='load model')
+model_path_widget = ipywidgets.Text(description='model path', value='../data/classification/my_model.pth')
+
+def load_model(c):
+    model.load_state_dict(torch.load(model_path_widget.value))
+model_load_button.on_click(load_model)
+    
+def save_model(c):
+    torch.save(model.state_dict(), model_path_widget.value)
+model_save_button.on_click(save_model)
+
+model_widget = ipywidgets.VBox([
+    model_path_widget,
+    ipywidgets.HBox([model_load_button, model_save_button])
+])
+
+# display(model_widget)
+print("model configured and model_widget created")
+
+import threading
+import time
+from utils import preprocess
+import torch.nn.functional as F
+
+state_widget = ipywidgets.ToggleButtons(options=['stop', 'live'], description='state', value='stop')
+prediction_widget = ipywidgets.Text(description='prediction')
+
+warning_widget = ipywidgets.Text(description='...')
+
+score_widgets = []
+for category in dataset.categories:
+    score_widget = ipywidgets.FloatSlider(min=0.0, max=1.0, description=category, orientation='vertical')
+    score_widgets.append(score_widget)
+
+def live(state_widget, model, camera, prediction_widget, score_widget):
+    global dataset
+    while state_widget.value == 'live':
+        image = camera.value
+        preprocessed = preprocess(image)
+        output = model(preprocessed)
+        output = F.softmax(output, dim=1).detach().cpu().numpy().flatten()
+        category_index = output.argmax()
+        prediction_widget.value = dataset.categories[category_index]
+        for i, score in enumerate(list(output)):
+            score_widgets[i].value = score
+            
+            # Customize output channels here
+            if score >= 0.5:
+                if i == 0:
+                    # No mask protocol in this block
+                    warning_widget.description = 'Put on a mask!'
+                    
+                elif i == 1:
+                    # Bad mask protocol in this block
+                    warning_widget.description = 'Fix your mask!'
+                    
+                elif i == 2:
+                    # Good mask protocol in this block
+                    warning_widget.description = 'Well done!'
+                    
+                else:
+                    # Undecided prodocol in this block
+                    warning_widget.description = '...'
+            
+            
+def start_live(change):
+    if change['new'] == 'live':
+        execute_thread = threading.Thread(target=live, args=(state_widget, model, camera, prediction_widget, score_widget))
+        execute_thread.start()
+
+state_widget.observe(start_live, names='value')
+
+live_execution_widget = ipywidgets.VBox([
+    ipywidgets.HBox(score_widgets),
+    prediction_widget,
+    state_widget,
+    warning_widget
+])
+
+# display(live_execution_widget)
+print("live_execution_widget created")
+
+BATCH_SIZE = 8
+
+optimizer = torch.optim.Adam(model.parameters())
+# optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+
+epochs_widget = ipywidgets.IntText(description='epochs', value=1)
+eval_button = ipywidgets.Button(description='evaluate')
+train_button = ipywidgets.Button(description='train')
+loss_widget = ipywidgets.FloatText(description='loss')
+accuracy_widget = ipywidgets.FloatText(description='accuracy')
+progress_widget = ipywidgets.FloatProgress(min=0.0, max=1.0, description='progress')
+
+def train_eval(is_training):
+    global BATCH_SIZE, LEARNING_RATE, MOMENTUM, model, dataset, optimizer, eval_button, train_button, accuracy_widget, loss_widget, progress_widget, state_widget
+    
+    try:
+        train_loader = torch.utils.data.DataLoader(
+            dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=True
+        )
+
+        state_widget.value = 'stop'
+        train_button.disabled = True
+        eval_button.disabled = True
+        time.sleep(1)
+
+        if is_training:
+            model = model.train()
+        else:
+            model = model.eval()
+        while epochs_widget.value > 0:
+            i = 0
+            sum_loss = 0.0
+            error_count = 0.0
+            for images, labels in iter(train_loader):
+                # send data to device
+                images = images.to(device)
+                labels = labels.to(device)
+
+                if is_training:
+                    # zero gradients of parameters
+                    optimizer.zero_grad()
+
+                # execute model to get outputs
+                outputs = model(images)
+
+                # compute loss
+                loss = F.cross_entropy(outputs, labels)
+
+                if is_training:
+                    # run backpropogation to accumulate gradients
+                    loss.backward()
+
+                    # step optimizer to adjust parameters
+                    optimizer.step()
+
+                # increment progress
+                error_count += len(torch.nonzero(outputs.argmax(1) - labels).flatten())
+                count = len(labels.flatten())
+                i += count
+                sum_loss += float(loss)
+                progress_widget.value = i / len(dataset)
+                loss_widget.value = sum_loss / i
+                accuracy_widget.value = 1.0 - error_count / i
+                
+            if is_training:
+                epochs_widget.value = epochs_widget.value - 1
+            else:
+                break
+    except e:
+        pass
+    model = model.eval()
+
+    train_button.disabled = False
+    eval_button.disabled = False
+    state_widget.value = 'live'
+    
+train_button.on_click(lambda c: train_eval(is_training=True))
+eval_button.on_click(lambda c: train_eval(is_training=False))
+    
+train_eval_widget = ipywidgets.VBox([
+    epochs_widget,
+    progress_widget,
+    loss_widget,
+    accuracy_widget,
+    ipywidgets.HBox([train_button, eval_button])
+])
+
+# display(train_eval_widget)
+print("trainer configured and train_eval_widget created")
+
+# Uncomment code below for training interface
+
+all_widget = ipywidgets.VBox([
+    ipywidgets.HBox([data_collection_widget, live_execution_widget]), 
+    train_eval_widget,
+    model_widget
+])
+
+# Uncomment code below for result interface
+
+#sqr_camera_widget = ipywidgets.VBox([ipywidgets.HBox([camera_widget])])
+#all_widget = ipywidgets.VBox([ipywidgets.HBox([sqr_camera_widget, live_execution_widget])])
+
+# Display widgets
+display(all_widget)
+
+# Attention!  Execute this cell before moving to another notebook
+# The USB camera application only requires that the notebook be reset
+# The CSI camera application requires that the 'camera' object be specifically released
+
+import os
+import IPython
+
+if type(camera) is CSICamera:
+    print("Ignore 'Exception in thread' tracebacks\n")
+    camera.cap.release()
+
+os._exit(00)
